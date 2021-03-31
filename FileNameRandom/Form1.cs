fadd435299048bfc93085768b693eadf2de61bb8
SitronX <citronovafuska@gmail.com>
2021-03-31 20:02:23 +0200

    Initial Commit

---
+++ FileNameRandom/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileNameRandom
+{
+    public partial class Form1 : Form
+    {
+        string[] files;
+        string path = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    files = Directory.GetFiles(fbd.SelectedPath);
+                    textBox1.Text = fbd.SelectedPath;
+                    path = fbd.SelectedPath;
+
+
+                    System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int counter = 1;
+
+            foreach(string i in files)
+            {
+                string name =path+"\\"+ counter + "." + i.Split('.').Last();
+                counter++;
+
+                System.IO.File.Move(i, name);
+            }
+
+            MessageBox.Show("Success", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
